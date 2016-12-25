b28de49540d63e42e20f5a06a346b65730ddef13
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-12-25 23:00:36 +0200

    Initial Commit

---
+++ get_json_swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  get_json_swift3
+//
+//  Created by Yan Malinovsky on 25.12.16.
+//  Copyright © 2016 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
